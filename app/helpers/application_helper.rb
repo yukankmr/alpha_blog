36ab3627be93f98acb421f4e0314e214eb7ecc4d
yukankmr <ynakamura.jpn@gmail.com>
2022-05-19 09:04:39 +0000

    Add auth helper methods and modify nav partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  
   def gravatar_for(user, options = { size: 80 })
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
@@ -6,4 +7,13 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block")
   end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
 end
