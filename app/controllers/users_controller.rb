943c47b2fa68217c31c39aeb15afa170201d9b75
yukankmr <ynakamura.jpn@gmail.com>
2022-05-20 06:43:23 +0000

    Add functionality for users to delete their own accounts and associated articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :show, :update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :show, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def new
     @user = User.new
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles were successfully deleted"
+    redirect_to root_path
   end
   
   private
